578c507fe1652f2ea04b8855cd69b8cbdd91f11b
Stefan Schaefer <stefan@invis-server.org>
2014-01-03 18:45:30 +0100

    Syntaxfehler in charset.sql (etherpad) behoben.

--- usr/share/doc/packages/invis-setup/examples/etherpad/charset.sql
+++ usr/share/doc/packages/invis-setup/examples/etherpad/charset.sql
@@ -1,4 +1,4 @@
-alter database 'etherpad' character set utf8 collate utf8_bin;
 use 'etherpad';
+alter database character set utf8 collate utf8_bin;
 alter table 'store' convert to character set utf8 collate utf8_bin;
 
